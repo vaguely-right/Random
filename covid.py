7cc5a3e80bb35727ccd8b5e23c3f488e2b3fc090
Steve Lyster <steven.lyster@gmail.com>
2020-03-15 20:39:38 -0600

    Update covid.py

--- covid.py
+++ covid.py
@@ -115,9 +115,21 @@ ob = ob.transpose()
 ob.drop(['China'],axis=1,inplace=True)
 ob.plot(logy=True)
 
+#%%
+import seaborn as sns
+sns.set()
 
+# Load the iris dataset
+iris = sns.load_dataset("iris")
 
+# Plot sepal width as a function of sepal_length across days
+g = sns.lmplot(x="sepal_length", y="sepal_width", hue="species",
+               height=5, data=iris)
 
+# Use more informative axis labels than are provided by default
+g.set_axis_labels("Sepal length (mm)", "Sepal width (mm)")
+
+p = sns.lmplot(x='index',y='Alberta',data=can)
 
 
 
